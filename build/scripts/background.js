4192e2c6036d7a6fc69ee39c77858b948dd95fc9
Altair J. Fernandes <tatosjb@gmail.com>
2020-02-11 00:37:19 -0300

    remove build folder

--- build/scripts/background.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({
-    color: '#3aa757'
-  }, function () {
-    console.log('The color is green!!');
-  });
-});
