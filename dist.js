bca5108d6a8e5bbaef8980257c75157e0810380e
Altair J. Fernandes <con@tato.dev>
2020-04-09 17:21:38 -0300

    Update dist.js

--- dist.js
+++ dist.js
@@ -1,4 +1,5 @@
 const execSync = require('child_process').execSync
+const spawnSync = require('child_process').spawnSync
 const fs = require('fs-extra')
 const AdmZip = require('adm-zip')
 const zip = new AdmZip()
@@ -6,7 +7,7 @@ const manifestPath = './src/manifest.json'
 const manifest = fs.readJSONSync(manifestPath)
 const semver = require('semver')
 
-function getReleaseType (args) {
+function getReleaseType(args) {
   if (args.includes('patch')) return 'patch'
   if (args.includes('minor')) return 'minor'
   if (args.includes('major')) return 'major'
@@ -22,10 +23,15 @@ manifest.version = semver.inc(manifest.version, releaseType)
 console.log('Generating version', manifest.version)
 fs.writeFileSync(manifestPath, Buffer.from(JSON.stringify(manifest, null, 2)))
 
-zip.addLocalFolder('./build')
-fs.ensureDirSync('./packages')
-zip.writeZip(`./packages/Version-${manifest.version}.zip`)
-
 execSync(`git add src/manifest.json && git commit -m 'Version-${manifest.version}'`, {
   cwd: process.cwd()
 })
+
+const child = spawnSync('yarn', ['build', '--color=always'])
+zip.addLocalFolder('./build')
+
+fs.ensureDirSync('./packages')
+zip.writeZip(`./packages/Version-${manifest.version}.zip`)
+
+if (child.stdout) console.log(child.stdout.toString())
+if (child.stderr) console.log(child.stderr.toString())
